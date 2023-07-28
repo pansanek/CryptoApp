59e8f0c9a5208398883bdb722528f81478202be4
SashaPotemkin <alex.potjomkin@gmail.com>
2023-07-28 21:58:26 +0300

    Работа с Data-слоем

--- app/src/main/java/com/example/cryptoapp/data/database/CoinInfoDao.kt
+++ app/src/main/java/com/example/cryptoapp/data/database/CoinInfoDao.kt
@@ -5,16 +5,15 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.example.cryptoapp.data.model.CoinPriceInfo
 
 @Dao
-interface CoinPriceInfoDao {
+interface CoinInfoDao {
     @Query("SELECT * FROM full_price_list ORDER BY lastUpdate DESC")
-    fun getPriceList(): LiveData<List<CoinPriceInfo>>
+    fun getPriceList(): LiveData<List<CoinInfoDBModel>>
 
     @Query("SELECT * FROM full_price_list WHERE fromSymbol == :fSym LIMIT 1")
-    fun getPriceInfoAboutCoin(fSym: String): LiveData<CoinPriceInfo>
+    fun getPriceInfoAboutCoin(fSym: String): LiveData<CoinInfoDBModel>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertPriceList(priceList: List<CoinPriceInfo>)
+    fun insertPriceList(priceList: List<CoinInfoDBModel>)
 }
