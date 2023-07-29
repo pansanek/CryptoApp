5a703d672f51bd7fcd6ecaa153d3d57795d72b85
SashaPotemkin <alex.potjomkin@gmail.com>
2023-07-29 15:27:57 +0300

    Use view binding in adapter

---
+++ app/src/main/java/com/example/cryptoapp/presentation/adapters/CoinInfoViewHolder.kt
@@ -0,0 +1,9 @@
+package com.example.cryptoapp.presentation.adapters
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cryptoapp.databinding.ItemCoinInfoBinding
+
+class CoinInfoViewHolder(
+    val binding: ItemCoinInfoBinding
+)
+    : RecyclerView.ViewHolder(binding.root)
