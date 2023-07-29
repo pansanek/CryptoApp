1cdfe59f0761ba81d02ee1d46c3747e7d4a7b873
SashaPotemkin <alex.potjomkin@gmail.com>
2023-07-29 15:56:33 +0300

    Фоновая загрузка данных. WorkManager

---
+++ app/src/main/java/com/example/cryptoapp/data/workers/RefreshDataWorker.kt
@@ -0,0 +1,41 @@
+package com.example.cryptoapp.data.workers
+
+import android.content.Context
+import androidx.work.*
+import com.example.cryptoapp.data.database.AppDatabase
+import com.example.cryptoapp.data.mapper.CoinMapper
+import com.example.cryptoapp.data.network.ApiFactory
+import kotlinx.coroutines.delay
+
+class RefreshDataWorker(context:Context,workerParameters: WorkerParameters):
+    CoroutineWorker(context,workerParameters) {
+    private val coinInfoDao = AppDatabase.getInstance(context).coinInfoDao()
+
+    private val mapper = CoinMapper()
+
+    private val apiService = ApiFactory.apiService
+    override suspend fun doWork(): Result {
+        while(true) {
+            try {
+                val topCoins = apiService.getTopCoinsInfo(limit = 50)
+                val fSymbol = mapper.mapNamesListToString(topCoins)
+                val jsonContainter = apiService.getFullPriceList(fSyms = fSymbol)
+                val coinInfoDtoList = mapper.mapJsonContainerToListCoinInfo(jsonContainter)
+                val dbModelList = coinInfoDtoList.map { mapper.mapDtoToDbModel(it) }
+                coinInfoDao.insertPriceList(dbModelList)
+            } catch (e: Exception) {
+            }
+            delay(1000)
+        }
+    }
+
+    companion object{
+        const val NAME ="RefreshDataWorker"
+
+
+        fun makeRequest():OneTimeWorkRequest{
+            return OneTimeWorkRequestBuilder<RefreshDataWorker>().build()
+        }
+    }
+
+}
