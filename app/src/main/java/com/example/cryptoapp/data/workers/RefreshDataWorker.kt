927dafda1f60ffbf78ccc72ab92c8f82f1ed9852
SashaPotemkin <alex.potjomkin@gmail.com>
2023-08-02 17:06:17 +0300

    DependencDependency Injection into WorkerFactory (Single Worker)

--- app/src/main/java/com/example/cryptoapp/data/workers/RefreshDataWorker.kt
+++ app/src/main/java/com/example/cryptoapp/data/workers/RefreshDataWorker.kt
@@ -3,19 +3,23 @@ package com.example.cryptoapp.data.workers
 import android.content.Context
 import androidx.work.*
 import com.example.cryptoapp.data.database.AppDatabase
+import com.example.cryptoapp.data.database.CoinInfoDao
 import com.example.cryptoapp.data.mapper.CoinMapper
 import com.example.cryptoapp.data.network.ApiFactory
+import com.example.cryptoapp.data.network.ApiService
 import kotlinx.coroutines.delay
 
-class RefreshDataWorker(context:Context,workerParameters: WorkerParameters):
-    CoroutineWorker(context,workerParameters) {
-    private val coinInfoDao = AppDatabase.getInstance(context).coinPriceInfoDao()
+class RefreshDataWorker(
+    context: Context,
+    workerParameters: WorkerParameters,
+    private val coinInfoDao: CoinInfoDao,
+    private val apiService: ApiService,
+    private val mapper: CoinMapper,
+):
+    CoroutineWorker(context, workerParameters) {
 
-    private val mapper = CoinMapper()
-
-    private val apiService = ApiFactory.apiService
     override suspend fun doWork(): Result {
-        while(true) {
+        while (true) {
             try {
                 val topCoins = apiService.getTopCoinsInfo(limit = 50)
                 val fSymbol = mapper.mapNamesListToString(topCoins)
@@ -29,11 +33,11 @@ class RefreshDataWorker(context:Context,workerParameters: WorkerParameters):
         }
     }
 
-    companion object{
-        const val NAME ="RefreshDataWorker"
+    companion object {
+        const val NAME = "RefreshDataWorker"
 
 
-        fun makeRequest():OneTimeWorkRequest{
+        fun makeRequest(): OneTimeWorkRequest {
             return OneTimeWorkRequestBuilder<RefreshDataWorker>().build()
         }
     }
